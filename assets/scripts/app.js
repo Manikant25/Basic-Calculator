6ff1a1669a55c39d3455bcbe733b5a9ba7aab4ae
Manikant Rai <manikant.rai2000@gmail.com>
2021-03-10 01:13:34 +0530

    add function added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,25 +1,13 @@
 const defaultResult=0;
 let currentResult=defaultResult // Variable delcaration
 
-function add(num1,num2){
-  result = num1 + num2;
-//  alert(`The result is ${result}`)
-return 
-alert("It will not run as it is after return ")
+function add(){
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult,'');
 }
 
-additonResult = add(100,200)
-alert(`Result is ${additonResult}`)
-// add(10,10)
-// add(10,20)
-
-currentResult = currentResult + 10
-
-//let calculationDescription='(' + currentResult + '+10) * 23'; //Js will take it as string
-//Better way for string concatenation 
-let calculationDescription =`(${defaultResult} +10) * 10 -12` //Backtiks and dollar sign use in string
  
+addBtn.addEventListener("click",add);
 
 
-outputResult(currentResult,calculationDescription);
 
