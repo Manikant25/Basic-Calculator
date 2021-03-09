3b37fd4889ea07701ffacefa3f27b80616c2a6bf
Manikant Rai <manikant.rai2000@gmail.com>
2021-03-10 01:43:47 +0530

    all button and functionality added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,53 @@
 const defaultResult=0;
 let currentResult=defaultResult // Variable delcaration
 
-function add(){
-  currentResult = currentResult + userInput.value;
-  outputResult(currentResult,'');
+function getUserInput (){
+return parseInt(userInput.value)
 }
 
- 
+function createAndWriteLog(opertor ,resultBeforeca, calcNumber)
+{
+    const calcDescription = `${resultBeforeca} ${opertor} ${calcNumber}`; 
+    outputResult(currentResult,calcDescription); 
+}
+
+function add(){     
+    const enteredNumber = getUserInput()
+    //It is a string , whatever input we take it will be in string
+const initailResult = currentResult
+  currentResult = currentResult + enteredNumber ; // currentResult = currentResult + +usernput.value;  This is the another way to convert
+  createAndWriteLog('+',initailResult,enteredNumber)
+}
+
+function substract(){
+    const enteredNumber = getUserInput()
+    //It is a string , whatever input we take it will be in string
+    const initailResult = currentResult
+    currentResult = currentResult - enteredNumber ; // currentResult = currentResult + +usernput.value;  This is the another way to convert
+    createAndWriteLog('-',initailResult,enteredNumber)
+}
+
+function multiply () {
+    const enteredNumber = getUserInput()
+    //It is a string , whatever input we take it will be in string
+    const initailResult = currentResult
+    currentResult = currentResult * enteredNumber ; // currentResult = currentResult + +usernput.value;  This is the another way to convert
+    createAndWriteLog('*',initailResult,enteredNumber)
+}
+
+function divide (){
+    const enteredNumber = getUserInput()
+    //It is a string , whatever input we take it will be in string
+    const initailResult = currentResult
+    currentResult = currentResult / enteredNumber ; // currentResult = currentResult + +usernput.value;  This is the another way to convert
+    createAndWriteLog('/',initailResult,enteredNumber)
+
+}
+
 addBtn.addEventListener("click",add);
+subtractBtn.addEventListener("click",substract);
+multiplyBtn.addEventListener("click",multiply);
+divideBtn.addEventListener("click",divide);
 
 
 
